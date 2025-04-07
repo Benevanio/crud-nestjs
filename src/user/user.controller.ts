4363c00464f0ed7d101e461a7f73a5261475f910
Benevanio <benevaniosantos930@gmail.com>
2025-04-07 20:31:13 -0300

    feat: adicionar método GET no controlador User para obter usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   createUser(@Body() body: Record<string, any>) {
+    return { body };
+  }
+  @Get()
+  getUser(@Body() body: Record<string, any>) {
     return { body };
   }
 }
